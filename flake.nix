1419dfbbc9974b763d71641adafc705f58b360ff
LavaDesu <me@lava.moe>
2022-10-23 01:49:10 +0700

    chore: ci and nix building

--- flake.nix
+++ flake.nix
@@ -3,23 +3,33 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem(system:
     let
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs {
         inherit system overlays;
       };
+      toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+      naersk' = pkgs.callPackage naersk {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
     in {
+      defaultPackage = naersk'.buildPackage {
+        src = ./.;
+      };
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           pkg-config
           openssl
-          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          toolchain
         ];
       };
     });
